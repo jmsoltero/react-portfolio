e610a66f08bdfffd7fbf1a95d2afda8956b9d9ab
josemanuelsoltero <josemanuelsoltero@joses-MacBook-Pro.local>
2023-04-22 14:25:20 -0500

    added about me

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavigationBar from './screens/navigationbar/NavigationBar';
 import Particles from './Particles';
 import Header from './screens/header/Header';
+import AboutMe  from './screens/aboutMe/aboutMe';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       < NavigationBar />
       < Header />
       < Particles />
+      < AboutMe />
   </div>
   );}
 
--- src/screens/navigationbar/NavigationBar.js
+++ src/screens/navigationbar/NavigationBar.js
@@ -13,7 +13,10 @@ function createLinks(){
 export default function NavigationBar() {
   return (
     <div id='home'>
-        < Navbar className='navigation-container' expand='md'>
+        < Navbar className='navigation-container' 
+        style={ {zIndex: '2', position: 'fixed', top: 0, width: '100%'}}
+        collapseOnSelect
+        expand='md'>
         <Navbar.Brand style={{ marginLeft:'1rem'}} href='#home'>Manuel Soltero</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav'/>
         <Navbar.Collapse style={{ justifyContent:'flex-end', marginRight:'1rem', borderColor:'none'}}>
