b349e397fb2badaa1557441580b469bd35d037e2
josemanuelsoltero <josemanuelsoltero@joses-MBP.lan>
2023-04-26 02:42:03 -0500

    added projects

---
+++ src/helpers/portfolioData.js
@@ -0,0 +1,42 @@
+import p1 from '../images/P_screenshots/p1.png'
+import p2 from '../images/P_screenshots/p2.png'
+import p3 from '../images/P_screenshots/p3.png'
+import p4 from '../images/P_screenshots/p4.png'
+
+
+
+
+
+const portfolioData = [
+    {
+        image: p1,
+        link: 'https://adblow-team-1.github.io/Project-ADBLOW/Links',
+        title: 'A.D.B.L.O.W',
+        summary: 'This project was one of my first experiences working with an api and working with a team',
+        tech: 'CSS,HTML, and JS/express/nodejs'
+
+    },
+    {
+        image: p2,
+        link: 'https://sheltered-falls-73233.herokuapp.com/',
+        title: 'Princess Saving Sim',
+        summary: 'This project was my first experience working on a game-type project, was very fun',
+        tech: 'CSS,HTML/canvas, and JS/express/nodejs/handlebars'
+    },
+    {
+        image: p3,
+        link: 'https://jmsoltero.github.io/Mod5-challenge-joses-planner/',
+        title: 'Schedule App',
+        summary: 'This project I used local storage and it was one of my first experiences with js',
+        tech: 'CSS,HTML, and JS'
+    },
+    {
+        image: p4,
+        link: 'https://jmsoltero.github.io/forecast/',
+        title: 'Weather API',
+        summary: 'This project was one of my first experiences working with an api',
+        tech: 'CSS,HTML, and JS/express/nodejs'
+    }
+];
+
+export default portfolioData
---
+++ src/screens/portfolio/Portfolio.js
@@ -0,0 +1,80 @@
+import React, { useState} from 'react'
+import './portfolio.css'
+import portfolioData from '../../helpers/portfolioData'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Card from 'react-bootstrap/Card'
+import Image from 'react-bootstrap/Image'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+
+
+function Portfolio() {
+  const [modalShow,setModalShow] = useState(false)
+  const [tempData, setTempData] = useState({})
+
+ const handleClose = () => setModalShow(false);
+  function createModal(data) {
+    return(
+      < Modal
+      show={modalShow}
+      onHide={()=>{setModalShow(false)}}
+      size='lg'
+      arial-labelledby="contained-modal-title-vcenter"
+      centered 
+      >
+      <Modal.Header>
+        <Modal.Title id='contained-modal-title-vcenter'>
+          {data.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>{data.summary}</p>
+        <Image src={data.image} style={{width:'200px'}}/>
+      </Modal.Body>
+      <a id='portfolio_modal_link' href={data.link} target='_blank' rel='noreferrer'> Check it out</a>
+      <Modal.Footer>
+        <div>Tech Used: </div>
+        <p style={{fontSize: '0.7rem', marginRight: 'auto'}}>{data.tech}</p>
+        <Button onClick={()=>{setModalShow(false)}}>Exit</Button>
+      </Modal.Footer>
+      </Modal>
+    )
+  }
+
+  const mapped = portfolioData.map((e,idx) => {
+    return(
+      < Card key={idx} id='portfolio_card_container'>
+        < Image className='portfolio_image'
+        onClick={() => {
+          setTempData({
+            image: e.image,
+            link: e.link,
+            title: e.title,
+            summary: e.summary,
+            tech: e.tech
+          })
+          setModalShow(true)
+        }}
+        src={e.image}/>
+        <div className='click_info'>&#x1F50E;&#xFE0E;</div>
+        {createModal(tempData)}
+      </Card>
+    )
+  })
+
+  return (
+    <div className='portfolio_main_container' id='portfolio'>
+      <h1>Portfolio</h1>
+    <p>Github: <a href='https://github.com/jmsoltero' target='_blank' rel='noreferrer'>https://github.com/jmsoltero</a>
+    </p>
+    <Container fluid='lg' style={{ padding: '2rem 0'}}>
+      <Row>
+        {mapped}
+      </Row>
+    </Container>
+    </div>
+  )
+}
+
+export default Portfolio
